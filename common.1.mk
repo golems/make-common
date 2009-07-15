de7a3c27386131a6a2a9aa890829512ddf8d6577
Neil T. Dantam <ntd@gatech.edu>
2009-07-15 02:35:20 +0000

    make-common: dependency generation, default variables, separate   build/src/include dires
    git-svn-id: file:///home/svn/humanoids/make-common@1237 4f504ea5-f356-0410-adec-f1b3679f3bb7

--- common.1.mk
+++ common.1.mk
@@ -2,6 +2,10 @@
 ## AUTHOR: Neil Dantam
 ##
 ## Some common routines for makefiles
+##
+## Almost surely requires gmake.
+## You should use a GNU userland.
+
 
 ## USAGE:
 #
@@ -13,23 +17,11 @@
 # # Project Version
 # VERSION := 0.0
 #
-# # root to install to for `make install'
-# PREFIX := /usr/local
-#
-# # Directory containing C/C++ headers
-# INCLUDEDIR := .
-#
 # # Binary Files
 # BINFILES := helloexample
 #
 # # Library files
 # LIBFILES := libhelloexample.so
-#
-# # Directory of files (or links to files) to copy verbatim
-# VERBATIMDIR := verbatim
-#
-# # if you use stow, root of your stow package directory
-# STOWBASE := /usr/local/stow
 #
 # # Files to tar up for distribution
 # DISTFILES := hello.c hello.h
@@ -39,25 +31,279 @@
 #
 # # Path to copy HTML Doxygen documentation
 # DOXPATH := $(HOME)/prism/public_html/dox
-#
-# # Compiler and linker to use
-# cc := gcc
-# f95 := gfortran
-# ld := ld
 
+
+#######################
+## DEFAULT VARIABLES ##
+#######################
+
+## Override these prior to the include as desired
+
+## define compilers
+
+# C Compiler
+ifndef cc
+cc := gcc
+endif
+
+# C++ Compiler
+ifndef CC
+CC = g++
+endif
+
+# Fortran 95 Compiler
+ifndef f95
+f95 := gfortran
+endif
+
+
+# Objective C Compiler
+ifndef objcc
+objcc := gcc
+endif
+
+# Static Libary Linker
+ifndef ar
+ar := ar
+endif
+
+# Linker
+ifndef ld
+ld := ld
+endif
+
+## default directories
+
+ifndef INCLUDEDIR
+# ./include if it exists, else .
+INCLUDEDIR := $(shell if [ -d ./include ]; then echo ./include; else echo .; fi)
+endif
+
+ifndef SRCDIR
+# ./src if it exists, else .
+SRCDIR := $(shell if [ -d ./src ]; then echo ./src; else echo .; fi)
+endif
+
+
+ifndef BUILDDIR
+# ./src if it exists, else .
+BUILDDIR := $(shell if [ -d ./build ]; then echo ./build; else echo .; fi)
+endif
+
+ifndef LIBDIR
+# ./src if it exists, else .
+LIBDIR := $(shell if [ -d $(BUILDDIR)/lib ]; then echo $(BUILDDIR)/lib; else echo $(BUILDDIR); fi)
+endif
+
+ifndef BINDIR
+# ./src if it exists, else .
+BINDIR := $(shell if [ -d $(BUILDDIR)/bin ]; then echo $(BUILDDIR)/bin; else echo $(BUILDDIR); fi)
+endif
+
+ifndef LIBDIRS
+LIBDIRS = $(LIBDIR)
+endif
+
+ifndef PREFIX
+PREFIX := /usr/local
+endif
+
+## default compiler flags
+
+# C
+ifndef CFLAGS
+CFLAGS := -g -I$(INCLUDEDIR)
+endif
+
+# C++
+ifndef CPPFLAGS
+CPPFLAGS = $(CFLAGS)
+endif
+
+
+# Objective-C
+ifndef OBJCFLAGS
+OBJCFLAGS = $(CFLAGS)
+endif
+
+
+# Fortran
+ifndef FFLAGS
+FFLAGS := -g -J$(INCLUDEDIR)
+endif
+
+# Linker
+ifndef LDFLAGS
+LDFLAGS := -shared
+endif
+
+## default source files
+
+ifndef SRCFILES
+SRCFILES := $(shell find .  \( -type d \( -name .svn -o -name .git \)  -prune \)  -o -regex '.*\.\(c\|cpp\|f95\)' -print)
+# Maybe svn list -R | egrep '^.*\.(c|cpp|f95)$'
+# It would be nicer (40x) on a cold disk cache, but slower (4x) otherwise
+# Would also fail if files are not yet svn add'ed
+endif
+
+ifndef VERBATIMDIR
+VERBATIMDIR := verbatim
+endif
+
+# if you use stow, root of your stow package directory
+ifndef STOWBASE
+STOWBASE := $(PREFIX)/stow
+endif
+
+comma := ,
 
 PROJVER := $(PROJECT)-$(VERSION)
 
 STOWDIR := $(PROJVER)
 STOWPREFIX := $(STOWBASE)/$(STOWDIR)
+DEPDIR := .deps
 
 
-define LINKLIB
-$(1): $(2)
-	$(ld) $(LDFLAGS) -o $(1) $(2)
+## Dependency Files
+## (gcc will generate dependency info for C and C++ files and spit out make rules)
+## We use one dep file for each source file to minimize dependency regenerations
+DEPFILES := $(addprefix $(DEPDIR)/,$(addsuffix .d, $(filter %.c %.cpp, $(SRCFILES))))
+
+#####################
+## FIXUP VARIABLES ##
+#####################
+
+LIBFILES := $(addprefix $(LIBDIR)/, $(LIBFILES))
+BINFILES := $(addprefix $(BINDIR)/, $(BINFILES))
+
+#######################
+## PORTABILITY TESTS ##
+#######################
+
+## OS X uses a different library extension, play nice
+ifeq ($(PLATFORM),Darwin)
+# somebody who can actually stomach "The Apple Way" should test this...
+SHARED_LIB_SUFFIX := .dylib
+LDFLAGS := -lc
+else
+SHARED_LIB_SUFFIX := .so
+endif
+
+
+
+
+#############
+## TARGETS ##
+#############
+
+## Target to print out the defined variables
+env:
+	@echo PROJECT: $(PROJECT)
+	@echo VERSION: $(VERSION)
+	@echo PREFIX: $(PREFIX)
+	@echo SRCDIR: $(SRCDIR)
+	@echo INCLUDEDIR: $(INCLUDEDIR)
+	@echo BUILDDIR: $(BUILDDIR)
+	@echo LIBDIR: $(LIBDIR)
+	@echo BINDIR: $(BINDIR)
+	@echo PLATFORM: $(PLATFORM)
+	@echo cc: $(cc)
+	@echo CC: $(CC)
+	@echo f95: $(f95)
+	@echo objcc: $(objcc)
+	@echo ld: $(ld)
+	@echo ar: $(ar)
+	@echo CFLAGS: $(CFLAGS)
+	@echo CPPFLAGS: $(CPPFLAGS)
+	@echo OBJCFLAGS: $(OBJCFLAGS)
+	@echo FFLAGS: $(FFLAGS)
+	@echo LDFLAGS: $(LDFLAGS)
+	@echo SRCFILES: $(SRCFILES)
+	@echo OBJFILES: $(OBJFILES)
+	@echo DEPFILES: $(DEPFILES)
+	@echo LIBFILES: $(LIBFILES)
+	@echo BINFILES: $(BINFILES)
+	@echo SHARED_LIB_SUFFIX: $(SHARED_LIB_SUFFIX)
+
+
+
+
+## Include the auto-generated dependencies
+-include $(DEPFILES)
+
+
+
+
+#####################
+## DEFAULT TARGETS ##
+#####################
+
+## These create targets to build C, C++, Fortran, and Objective C
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.c
+	@mkdir -vp $(dir $(@))
+	$(cc) $(CFLAGS) -c $< -o $@
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.cpp
+	@mkdir -vp $(dir $(@))
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.cc
+	@mkdir -vp $(dir $(@))
+	$(CC) $(CPPFLAGS) -c $< -o $@
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.f95
+	@mkdir -vp $(dir $(@))
+	$(f95) -J$(INCLUDEDIR) -c $< -o $@
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.m
+	@mkdir -vp $(dir $(@))
+	$(objcc) $(OBJCFLAGS) -c $< -o $@
+
+## Rules to generate dependecy info
+## Hopefully gfortran will do this too, soon
+
+$(DEPDIR)/%.c.d: $(SRCDIR)/%.c
+	@mkdir -pv $(dir $@)
+	echo $(dir $<)`$(cc) $(CFLAGS) -MM  $<` > $@
+
+$(DEPDIR)/%.cpp.d: $(SRCDIR)/%.cpp
+	@mkdir -pv $(dir $@)
+	echo $(dir $<)`$(cc) $(CPPFLAGS) -MM $<` > $@
+
+
+## Convenience method for linking shared libraries
+## For some reason, automatic variables don't work here...
+## call with  $(call LINKLIB, name_of_lib, list of object files)
+## ie with $(call LINKLIB, frob, foo.o bar.o) # gives libfrob.so
+define LINKLIB1
+$(LIBDIR)/lib$(strip $(1))$(SHARED_LIB_SUFFIX): $(2)
+	$(ld) $(LDFLAGS) -o $(LIBDIR)/lib$(strip $(1))$(SHARED_LIB_SUFFIX) $(2)
 endef
 
-default: $(BINFILES) $(LIBFILES)
+# this def does the eval so the caller doesn't have to
+define LINKLIB
+$(eval $(call LINKLIB1, $1, $2))
+endef
+
+#$(addprefix -llib, $(addsuffix $(4), .a))
+
+
+## Convenience method for linking binaries
+## call with $(call LINKLIB, name_of_binary, object files, shared libs, static libs)
+## ie  $(call LINKBIN frob, foo.o, bar, bif)
+define LINKBIN1
+$(strip $(1)): $(2)  $(addsuffix .a, $(addprefix lib, $(4)))
+	$(cc) $(CFLAGS) -o $(strip $(1)) $(2) \
+	  $(addprefix -L, $(LIBDIRS))  \
+	  $(if $(strip $(4)), -Wl$(comma)-Bstatic) $(addprefix -l, $(strip $(4)))  \
+	  $(if $(strip $(3)), -Wl$(comma)-Bdynamic) $(addprefix -l, $(3))
+endef
+
+# this def does the eval so the caller doesn't have to
+define LINKBIN
+$(eval $(call LINKBIN1, $1, $2, $3, $4))
+endef
 
 TERM_GREEN="\033[0;32m"
 TERM_NO_COLOR="\033[0m"
@@ -76,14 +322,14 @@ stow: default
 		install --mode 755 $(LIBFILES) $(STOWPREFIX)/lib; \
 	fi
 	@echo $(TERM_LIGHT_GREEN)'* INSTALLING HEADERS *'$(TERM_NO_COLOR)
-	if test -n "$(INCLUDEDIR)"; then \
+	if test -d "$(INCLUDEDIR)"; then \
 		mkdir -p $(STOWPREFIX)/include; \
 		cd $(INCLUDEDIR) && \
 		install --mode 644 `find -regex '^.*\.h'`\
 		  $(STOWPREFIX)/include; \
 	fi
 	@echo $(TERM_LIGHT_GREEN)'* INSTALLING VERBATIM *'$(TERM_NO_COLOR)
-	if test -n "$(VERBATIMDIR)"; then \
+	if test -d "$(VERBATIMDIR)"; then \
 		mkdir -p $(STOWPREFIX); \
 		cp -TLr $(VERBATIMDIR) $(STOWPREFIX); \
 	fi
