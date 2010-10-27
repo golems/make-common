dc63bca37e408e04fd4418434cfaf62f9b0d1637
Neil T. Dantam <ntd@gatech.edu>
2010-10-27 18:55:21 +0000

    shared makefile for 3rdparty autotools packages
    git-svn-id: file:///home/svn/humanoids/src/common/lib/make-common@5044 4f504ea5-f356-0410-adec-f1b3679f3bb7

---
+++ verbatim/share/make-common/common-3auto.mk
@@ -0,0 +1,81 @@
+## Shared Makefile to build 3rdparty libs via autotools
+##
+## -- BEGIN EXAMPLE --
+##
+## # Package name
+## MC3_PACKAGE := llvm
+##
+## # Package Version
+## MC3_VERSION := 2.8
+##
+## # URL to download package from
+## MC3_URL := http://llvm.org/releases/2.8/llvm-2.8.tgz
+##
+## # Other packages we depend on
+## MC3_DEPENDS := build-essential
+##
+## # Who to call when things break
+## MC3_MAINTAINER := George P. Burdell
+##
+## # Description of the package
+## MC3_DESCRIPTION := "Low-Level Virtual Machine (LLVM) compiler"
+##
+## # Now include the shared makefile
+## include /usr/share/make-common/common-3auto.mk
+##
+## # And now you just type make and it'll spit out a deb
+## -- END EXAMPLE --
+
+MC3_MAINTAINER ?= unknown
+MC3_DESCRIPTION ?= empty
+MC3_DEBVERSION ?= 1
+
+MC3_DEPENDS ?=
+
+ifndef MC3_TARBALL
+MC3_TARBALL ?= $(shell basename $(MC3_URL))
+endif
+
+ifndef MC3_SRCDIR
+MC3_SRCDIR ?= $(shell echo $(MC3_TARBALL) | sed -e 's!\.[^\.]*$$!!')
+endif
+
+MC3_DEB := $(MC3_PACKAGE)_$(MC3_VERSION)-$(MC3_DEBVERSION).deb
+
+
+
+$(MC3_DEB): $(MC3_SRCDIR) debian/DEBIAN/control
+	mkdir -p ./debian/usr
+	cd $< && ./configure --prefix=`pwd`/../debian/usr
+	cd $< && make
+	cd $< && make install
+	fakeroot dpkg-deb --build debian ./$@
+
+.PHONY: vars
+
+vars:
+	@echo MC3_URL: $(MC3_URL)
+	@echo MC3_TARBALL: $(MC3_TARBALL)
+	@echo MC3_SRCDIR: $(MC3_SRCDIR)
+	@echo MC3_DEB: $(MC3_DEB)
+
+$(MC3_TARBALL):
+	wget -c $(MC3_URL)
+
+$(MC3_SRCDIR): $(MC3_TARBALL)
+	tar xavf $(MC3_TARBALL)
+
+
+debian/DEBIAN/control:
+	mkdir -p debian/DEBIAN
+	echo Package: $(MC3_PACKAGE) > $@
+	echo Version: $(MC3_VERSION)-$(MC3_DEBVERSION) >> $@
+	echo Architecture: `uname -m | sed -e 's/i686/i386/; s/x86_64/amd64/'` >> $@
+	echo Maintainer: $(MC3_MAINTAINER) >> $@
+	echo Description: $(MC3_DESCRIPTION) >> $@
+	echo Depends: $(MC3_DEPENDS) >> $@
+
+
+clean:
+	rm -rf debian $(MC3_TARBALL) $(MC3_SRCDIR)
+
